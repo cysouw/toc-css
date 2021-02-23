830e042cd5309d30637c13c797d95efa052a8abb
Michael Cysouw <cysouw@mac.com>
2021-02-23 14:32:24 +0100

    first version

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # toc-css
-Pandoc Lua Filter to add table of contents to the side
+
+Pandoc Lua Filter to add floating table of contents to HTML
+
+**Michael Cysouw** <cysouw@mac.com>
+
+## Pandoc
+
+[Pandoc](https://pandoc.org) is a fabulous system to prepare text and export it to (almost) every format of your liking. Pandoc includes an option to add a table of contents, but by default this is just added at the top of the document. This Lua filter adds some better layout of the table of contents for HTML documents.
+
+The filter adds a bit CSS and vanilla Javascript to the HTML output as created by the Pandoc defaults. The actual html is not changed in any way. If you have not added a title for the table of contents then the title "CONTENTS" is automatically added.
+
+Please note that I am not a very proficient CSS/Javascript programmer. I welcome suggestions and improvements! The current version was specifically made for a long book with a very complex structure, the current version can be found [here](https://gitcdn.link/repo/cysouw/diathesis/main/cysouwDiathesisManuscript.html) as an example.
+
+## Usage
+
+- For this filter to work, the HTML export needs to have the options `--standalone` and `--toc` to produce the basic table of contents.
+- To add a title to the table of contents, either add the following to your metadata: `toc-title: MY-TITLE` or add the option `--metadat=toc-title:"MY-TITLE"` when producing the HTML.
+- The filter itself is added by using `--lua-filter toc-css.lua`.
+
+As an example, consider the following command to convert this `README.md` into `README.html`:
+
+```
+pandoc README.md \
+    --to html \
+    --output README.html \
+    --lua-filter toc-css.lua \
+    --standalone \
+    --toc \
+    --number-sections \
+    --metadata title="Introducing toc-css"
+```
+
+The remaining headers in this readme are only added to showcase the result in the HTML file.
+
+# Another first level heading
+
+## With subheading
+
+### Going deeper
+
+### More
+
+### Let's try a completely long and superfluous title
+
+## Another subheading
+
+### Example 1
+
+### Example 2
+
+### Example 3
+
+# Next Chapter
+
+## Next Subheading
+
+## Another very long title that will not fit
---
+++ toc-css.lua
@@ -0,0 +1,199 @@
+--[[
+toc-css.lua: lua script to add CSS and Vanilla Javascript to native Pandoc HTML output
+Formats HTML table of content as produced by Pandoc to the top left of the document
+
+Copyright © 2021 Michael Cysouw <cysouw@mac.com>
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+]]
+
+css = [[ 
+<!-- CSS added by filter 'toc-css.lua' for TOC hovering to the side -->
+<style>
+body {
+  padding-left: 1cm;
+  padding-right: 1cm;
+  transition: 0.5s;
+}
+nav {
+  width: 1em;
+  margin-left: -1cm;
+  font-size: smaller;
+  color: grey;
+  transition: 0.5s;
+  float: left;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  white-space: nowrap; 
+  overflow: hidden;
+  overflow-y: scroll;
+  transition: 0.5s;
+}
+nav::-webkit-scrollbar {
+  display: none;
+}
+nav a, nav a:visited {
+  color: grey;
+}
+nav h2:before {
+  content: "≡ ";
+  font-size: 150%;
+}
+nav h2:after {
+  content: " ◀";
+}
+nav li {
+  margin-left: -0.5em;
+  white-space: nowrap; 
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+nav li > a:not(:only-child):before {
+  content: "▶ ";
+}
+nav li > a:only-child {
+  margin-left: 0.75em;
+}
+nav li li {
+  margin-left: 1em;
+}
+nav li li li {
+  margin-left: -0.5em;
+  font-size: smaller;
+}
+nav ul li ul  {
+  visibility: hidden;
+  display: none;
+  margin-top: 0.2em;
+  margin-bottom: 0.2em;
+  transition: 0.5s;
+}
+.bodysmall {
+  padding-left: 9cm;
+  transition: 0.5s;
+}
+.navside {
+  width: 7cm;
+  margin-left: -8.5cm;
+  padding-right: 1cm;
+  transition: 0.5s;
+}
+.navside h2:after {
+  content: " ▶";
+}
+.navshown {
+  width: 50%;
+  transition: 0.5s;
+  background-color: rgba(255, 255, 255, 0.95);
+}
+.subShow > ul {
+  visibility: visible;
+  display: block;
+  transition: 0.5s;
+  margin-left: -1em;
+}
+.subShow > a:not(:only-child):before {
+  content: "▼ ";
+}
+</style>
+]]
+
+-----------------------------
+
+script = [[
+
+<!-- Javascript added by toc-css.lua to make TOC expandable on click -->
+<script>
+
+  const b = document.querySelector("body");
+  const n = document.querySelector("nav");
+  const buttonsize = 15;
+
+  // click on "toc-title" to show TOC to the side
+  document.querySelector("#toc-title").addEventListener("click", function(e) {
+    if (e.clientX < e.currentTarget.getBoundingClientRect().left + buttonsize) {
+      n.classList.toggle("navshown");
+    } else {
+      b.classList.toggle("bodysmall");
+      n.classList.toggle("navside");
+      n.classList.remove("navshown");
+    };
+  });
+
+  // by default show TOC in large window
+  window.onload = function() {
+    if (window.innerWidth > 1000) {
+      b.classList.add("bodysmall");
+      n.classList.add("navside");
+    };
+  };
+
+  // show/hide TOC on resize
+  window.onresize = function () {
+    if (window.innerWidth > 1000) {
+      b.classList.add("bodysmall");
+      n.classList.add("navside");
+    } else {
+      b.classList.remove("bodysmall");
+      n.classList.remove("navside");
+    };
+  };
+
+  // show/hide subsections
+  function toggleSub(lis) {
+    for (const li of lis) {
+      li.addEventListener('click', function (e) {
+        if (e.clientX < e.currentTarget.getBoundingClientRect().left + buttonsize) {
+          li.classList.toggle('subShow');
+          e.preventDefault();
+        };
+      });
+    };
+  };
+
+  const allLis = document.querySelectorAll("nav li");
+  toggleSub(allLis);
+
+</script>
+]]
+
+----------------------------
+
+function addCSS (meta)
+  -- read current "header-includes" from metadata, or make a new one
+  local current = meta['header-includes'] or pandoc.MetaList{meta['header-includes']}
+  -- add css to the end of "header-includes"
+  current[#current+1] = pandoc.MetaBlocks(pandoc.RawBlock("html", css))
+  meta['header-includes'] = current
+  if meta['toc-title'] == nil then
+    meta['toc-title'] = "CONTENTS"
+  end
+  -- return metadata
+  return(meta)
+end
+
+----------------------------
+
+function addScript (doc)
+  -- add javascript to the end of the document
+  table.insert(doc.blocks, pandoc.RawBlock("html", script) )
+  return(doc)
+end
+
+----------------------------
+
+return {
+  { Meta = addCSS },
+  { Pandoc = addScript }
+}
